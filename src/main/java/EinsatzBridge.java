53eb1b42bf22cf5ebde9d29d192aa35782391b2c
Finn Winter <22560479+Finn0811@users.noreply.github.com>
2020-01-24 15:20:35 +0100

    :tada: Initial commit

---
+++ src/main/java/EinsatzBridge.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Finn on 24.01.2020.
+ */
+public class EinsatzBridge {
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(EinsatzBridge.class);
+
+    public static void main(String[] args) {
+        logger.info("EinsatzBridge gestartet...");
+    }
+}
